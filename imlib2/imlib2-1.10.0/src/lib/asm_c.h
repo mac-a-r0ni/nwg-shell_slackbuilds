5e77743adeef64ca2d8e9d54e267bcb5b2a1a33f
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2022-12-19 23:39:11 -0500

    updates/fixes

--- imlib2/imlib2-1.10.0/src/lib/asm_c.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASM_C_H
-#define ASM_C_H 1
-
-#if defined(DO_MMX_ASM) || defined(DO_AMD64_ASM)
-int                 __imlib_do_asm(void);
-#endif
-
-#endif /* ASM_C_H */
--- imlib2/imlib2-1.10.0/src/lib/strutils.h
+++
@@ -1,7 +0,0 @@
-#ifndef STRUTILS_H
-#define STRUTILS_H
-
-char              **__imlib_StrSplit(const char *str, int delim);
-void                __imlib_StrSplitFree(char **plist);
-
-#endif /* STRUTILS_H */
