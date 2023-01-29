71f70a15b623011a45e64b0d06144da1a42cb6a0
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2023-01-29 11:18:58 -0500

    update nwg-shell 0.4.1

---
+++ build-cinnamon.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+
+# Copyright 2012  Patrick J. Volkerding, Sebeka, Minnesota, USA
+# All rights reserved.
+#
+# Copyright 2013 Chess Griffin <chess.griffin@gmail.com> Raleigh, NC
+# Copyright 2013-2019 Willy Sudiarto Raharjo <willysr@slackware-id.org>
+# All rights reserved.
+#
+# Based on the xfce-build-all.sh script by Patrick J. Volkerding
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Set to 1 if you'd like to install/upgrade package as they are built.
+# This is recommended.
+INST=1
+
+# This is where all the compilation and final results will be placed
+TMP=${TMP:-/tmp/csb}
+OUTPUT=${OUTPUT:-/tmp}
+
+# This is the original directory where you started this script
+CSBROOT=$(pwd)
+
+# Check for duplicate sources (default: OFF)
+CHECKDUPLICATE=0
+
+# Loop for all packages
+for dir in \
+  xdotool \
+  gsound \
+  libcroco \
+  pytz \
+  libtimezonemap \
+  tinycss \
+  setproctitle \
+  ptyprocess \
+  cjs \
+  python-pam \
+  cinnamon-desktop \
+  pangox-compat \
+  libgnomekbd \
+  xapp \
+  cinnamon-session \
+  libgusb \
+  colord \
+  cinnamon-settings-daemon \
+  gnome-menus \
+  cinnamon-menus \
+  cinnamon-control-center \
+  zenity \
+  cogl \
+  clutter \
+  clutter-gtk \
+  muffin \
+  caribou \
+  pexpect \
+  metacity \
+  polib \
+  cinnamon-translations \
+  nemo \
+  python3-xapp \
+  psutil \
+  cinnamon-screensaver \
+  mint-y-icons \
+  mint-themes \
+  cinnamon \
+  pia-manager \
+  ; do
+  # Get the package name
+  package=$(echo $dir | cut -f2- -d /)
+
+  # Change to package directory
+  cd $CSBROOT/$dir || exit 1
+
+  # Get the version
+  version=$(cat ${package}.SlackBuild | grep "VERSION:" | head -n1 | cut -d "-" -f2 | rev | cut -c 2- | rev)
+
+  # Get the build
+  build=$(cat ${package}.SlackBuild | grep "BUILD:" | cut -d "-" -f2 | rev | cut -c 2- | rev)
+
+  if [ $CHECKDUPLICATE -eq 1 ]; then
+    # Check for duplicate sources
+    sourcefile="$(ls -l $CSBROOT/$dir/${package}-*.tar.?z* | wc -l)"
+    if [ $sourcefile -gt 1 ]; then
+      echo "You have following duplicate sources:"
+      ls $CSBROOT/$dir/${package}-*.tar.?z* | cut -d " " -f1
+      echo "Please delete sources other than ${package}-$version to avoid problems"
+      exit 1
+    fi
+  fi
+
+  # The real build starts here
+  TMP=$TMP OUTPUT=$OUTPUT sh ${package}.SlackBuild || exit 1
+  if [ "$INST" = "1" ]; then
+    PACKAGE=`ls $OUTPUT/${package}-${version}-*-${build}*csb.txz`
+    if [ -f "$PACKAGE" ]; then
+      upgradepkg --install-new --reinstall "$PACKAGE"
+    else
+      echo "Error:  package to upgrade "$PACKAGE" not found in $OUTPUT"
+      exit 1
+    fi
+  fi
+
+  # back to original directory
+  cd $CSBROOT
+done
