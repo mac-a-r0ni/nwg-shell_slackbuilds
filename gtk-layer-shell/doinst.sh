1b3b2954186823504d2d43f016469e4e5a142c44
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2023-02-06 06:52:14 -0500

    adding SBo work

--- gtk-layer-shell/doinst.sh
+++
@@ -1,4 +0,0 @@
-( cd usr/lib64 ; rm -rf libgtk-layer-shell.so )
-( cd usr/lib64 ; ln -sf libgtk-layer-shell.so.0 libgtk-layer-shell.so )
-( cd usr/lib64 ; rm -rf libgtk-layer-shell.so.0 )
-( cd usr/lib64 ; ln -sf libgtk-layer-shell.so.0.7.0 libgtk-layer-shell.so.0 )
