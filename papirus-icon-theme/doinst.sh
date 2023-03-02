a5a8239da67a2ffb5387f2b35bd2e03f1e03f05d
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2023-03-02 02:25:38 -0500

    final papirus 20230301 update

--- papirus-icon-theme/doinst.sh
+++
@@ -1,8 +0,0 @@
-for theme in Papirus ePapirus ePapirus-Dark Papirus-Dark Papirus-Light; do
-  if [ -e usr/share/icons/$theme/icon-theme.cache ]; then
-    if [ -x /usr/bin/gtk-update-icon-cache ]; then
-      /usr/bin/gtk-update-icon-cache -f usr/share/icons/$theme >/dev/null 2>&1
-    fi
-  fi
-done
-
