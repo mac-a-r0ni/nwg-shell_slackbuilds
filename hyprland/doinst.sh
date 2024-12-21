41f04fed532de1abcafa55baf53aeec9a6852528
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2024-12-21 08:09:21 -0500

    hyprland: fix doinst.sh

--- hyprland/doinst.sh
+++
@@ -1,5 +0,0 @@
-# putting this here because it's a hyprland specific issue
-# hyprland will link against libdisplay-info 0.2.0 but unless it's symlinked
-# to the old lib, it fails to start
-( cd usr/lib64 ; ln -sf libdisplay-info.so.0.2.0 libdisplay-info.so.1 )
-
